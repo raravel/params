5d0e4a51dda2845056085328ad25b46ebc79eb9d
Tree Some <youn@tree-some.dev>
2020-06-17 10:29:38 +0900

    change when don't have query string, location.params [undefined -> {}]

--- params.js
+++ params.js
@@ -2,22 +2,19 @@ function __params_parse__() {
     var params = {};
     var qsIdx = location.href.indexOf('?');
 
-    if ( qsIdx < 0 ) {
-        return;
-    }
+    if ( qsIdx >= 0 ) {
+        var qs = location.href.substr(qsIdx);
+        var uri = decodeURI(qs);
+        var search = uri.replace(/^\?/, '');
+        var arr = search.split('&');
 
-    var qs = location.href.substr(qsIdx);
-	var uri = decodeURI(qs);
-    var search = uri.replace(/^\?/, '');
-    var arr = search.split('&');
-
-    for ( var i = 0;i< arr.length;i++ ) {
-        var ps = arr[i].split('=');
-        params[ps[0]] = ps[1] || '';
+        for ( var i = 0;i< arr.length;i++ ) {
+            var ps = arr[i].split('=');
+            params[ps[0]] = ps[1] || '';
+        }
     }
     
     location.params = params;
-
 };
 
 __params_parse__();
